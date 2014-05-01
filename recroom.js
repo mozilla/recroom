56aaa363d48d4dc19766f2741d68adc1ea7c0f71
Matthew Riley MacPherson <matt@lonelyvegan.com>
2014-05-01 19:04:41 -0400

    Don't show banner on run/serve

--- recroom.js
+++ recroom.js
@@ -24,28 +24,30 @@ if (opts.version) {
 }
 
 // Thanks to @fwenzel for this one.
-console.log(
-    "\n",
-    chalk.red("             _.-~-.\n"),
-    chalk.red("           7''  Q..\\\n"),
-    chalk.red("        _7         (_\n"),
-    chalk.red("      _7  _/    _q.  /\n"),
-    chalk.red("    _7 . ___  /VVvv-'_\n"),
-    chalk.red("   7/ / /~- \\_\\      '-._     .-'                      /       //\n"),
-    chalk.red("  ./ ( /-~-/||'=.__  '::. '-~'' {             ___   /  //     ./{\n"),
-    chalk.red(" V   V-~-~| ||   __''_   ':::.   ''~-~.___.-'' _/  // / {_   /  {  /\n"),
-    chalk.red("  VV/-~-~-|/ \\ .'__'. '.    '::                    "), chalk.white("_ _ _"), chalk.red("       ''.\n"),
-    chalk.red("  / /~~~~||VVV/ /  \\ )  \\       "), chalk.white("_ __ ___   ___ ___(_) | | __ _"), chalk.red("  .::'\n"),
-    chalk.red(" / (~-~-~\\.-' /    \\'   \\::::. "), chalk.white("| '_ ` _ \\ / _ \\_  / | | |/ _` |"), chalk.red(":::'\n"),
-    chalk.red("/..\\    /..\\__/      '     ':::"), chalk.white("| | | | | | (_) / /| | | | (_| |"), chalk.red("::'\n"),
-    chalk.red("vVVv    vVVv                 ':"), chalk.white("|_| |_| |_|\\___/___|_|_|_|\\__,_|"), chalk.red("''\n\n")
-);
+function banner() {
+    console.log(
+        "\n",
+        chalk.red("             _.-~-.\n"),
+        chalk.red("           7''  Q..\\\n"),
+        chalk.red("        _7         (_\n"),
+        chalk.red("      _7  _/    _q.  /\n"),
+        chalk.red("    _7 . ___  /VVvv-'_\n"),
+        chalk.red("   7/ / /~- \\_\\      '-._     .-'                      /       //\n"),
+        chalk.red("  ./ ( /-~-/||'=.__  '::. '-~'' {             ___   /  //     ./{\n"),
+        chalk.red(" V   V-~-~| ||   __''_   ':::.   ''~-~.___.-'' _/  // / {_   /  {  /\n"),
+        chalk.red("  VV/-~-~-|/ \\ .'__'. '.    '::                    "), chalk.white("_ _ _"), chalk.red("       ''.\n"),
+        chalk.red("  / /~~~~||VVV/ /  \\ )  \\       "), chalk.white("_ __ ___   ___ ___(_) | | __ _"), chalk.red("  .::'\n"),
+        chalk.red(" / (~-~-~\\.-' /    \\'   \\::::. "), chalk.white("| '_ ` _ \\ / _ \\_  / | | |/ _` |"), chalk.red(":::'\n"),
+        chalk.red("/..\\    /..\\__/      '     ':::"), chalk.white("| | | | | | (_) / /| | | | (_| |"), chalk.red("::'\n"),
+        chalk.red("vVVv    vVVv                 ':"), chalk.white("|_| |_| |_|\\___/___|_|_|_|\\__,_|"), chalk.red("''\n\n")
+    );
+}
 
 var command = opts.argv.remain[0];
 
 // Show the Mozilla dino banner.
 if (opts.banner) {
-    return;
+    return banner();
 }
 
 // Create a new project and initialize an app using generator-recroom.
@@ -65,7 +67,16 @@ if (command === 'new' || command === 'create') {
 
     shell.cd(projectName);
 
+    banner();
+
     spawn('yo', ['recroom'], {
+        stdio: 'inherit'
+    });
+}
+
+// Pipe out to grunt serve.
+if (command === 'run' || command === 'serve') {
+    spawn('grunt', ['serve'], {
         stdio: 'inherit'
     });
 }
