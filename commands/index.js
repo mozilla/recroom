e877b26f6d14094c68064438f819ccfd65350b7a
Brittany Storoz <brittanystoroz@gmail.com>
2014-10-02 18:23:40 -0400

    Expand if block

--- commands/index.js
+++ commands/index.js
@@ -5,14 +5,15 @@ var path = require('path');
 
 var commandFiles = fs.readdirSync(__dirname);
 
-
 commandFiles.forEach(function(file) {
-  var commandName = path.basename(file, '.js');
-  if (commandName === "index") { return; }
-  else {
-    var module = require("./" + file);
-    var moduleName = "cmd_" + commandName;
-    exports[moduleName] = module;
-  }
+    var commandName = path.basename(file, '.js');
+    if (commandName === "index") { 
+        return;
+    }
+    else {
+        var module = require("./" + file);
+        var moduleName = "cmd_" + commandName;
+        exports[moduleName] = module;
+    }
 });
 
