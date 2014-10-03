92b237cfdb6ed356cb79826f9cb1428001cb5742
Brittany Storoz <brittanystoroz@gmail.com>
2014-10-03 11:55:04 -0400

    Break dino banner out into separate file
    So it doesn't troll us with jscs styling rules.

--- recroom.js
+++ recroom.js
@@ -8,6 +8,7 @@ var chalk = require('chalk');
 var nopt = require('nopt');
 var shell = require('shelljs');
 var spawn = require('child_process').spawn;
+var banner = require('./banner');
 
 var binaryPath = __dirname + '/node_modules/.bin/';
 
@@ -26,26 +27,6 @@ var opts = nopt({
 if (opts.version) {
     console.log(chalk.blue('Rec Room, Version ' + VERSION));
     process.exit();
-}
-
-// Thanks to @fwenzel for this one.
-function banner() {
-    console.log(
-        "\n",
-        chalk.red("             _.-~-.\n"),
-        chalk.red("           7''  Q..\\\n"),
-        chalk.red("        _7         (_\n"),
-        chalk.red("      _7  _/    _q.  /\n"),
-        chalk.red("    _7 . ___  /VVvv-'_\n"),
-        chalk.red("   7/ / /~- \\_\\      '-._     .-'                      /       //\n"),
-        chalk.red("  ./ ( /-~-/||'=.__  '::. '-~'' {             ___   /  //     ./{\n"),
-        chalk.red(" V   V-~-~| ||   __''_   ':::.   ''~-~.___.-'' _/  // / {_   /  {  /\n"),
-        chalk.red("  VV/-~-~-|/ \\ .'__'. '.    '::                    "), chalk.white("_ _ _"), chalk.red("       ''.\n"),
-        chalk.red("  / /~~~~||VVV/ /  \\ )  \\       "), chalk.white("_ __ ___   ___ ___(_) | | __ _"), chalk.red("  .::'\n"),
-        chalk.red(" / (~-~-~\\.-' /    \\'   \\::::. "), chalk.white("| '_ ` _ \\ / _ \\_  / | | |/ _` |"), chalk.red(":::'\n"),
-        chalk.red("/..\\    /..\\__/      '     ':::"), chalk.white("| | | | | | (_) / /| | | | (_| |"), chalk.red("::'\n"),
-        chalk.red("vVVv    vVVv                 ':"), chalk.white("|_| |_| |_|\\___/___|_|_|_|\\__,_|"), chalk.red("''\n\n")
-    );
 }
 
 var command = opts.argv.remain[0];
