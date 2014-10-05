c64e403ad0a9c31e2972ffc400848d10873d9032
Brittany Storoz <brittanystoroz@gmail.com>
2014-10-05 06:49:26 -0700

    Remove IIFE from utils.js
    This was only needed because jshint was complaining about
    using a function form of 'use strict'. With node: true in
    our jshintrc, this complaint is irrelevant.

--- utils.js
+++ utils.js
@@ -1,36 +1,33 @@
-(function() {
-    'use strict';
+'use strict';
 
-    var fs = require('extfs');
+var fs = require('extfs');
 
-    module.exports = {
-        // Check app for bower and npm dependencies
-        checkForDeps: function(directories) {
-            this.getToRoot();
-            var dependenciesFound = true;
+module.exports = {
+    // Check app for bower and npm dependencies
+    checkForDeps: function(directories) {
+        this.getToRoot();
+        var dependenciesFound = true;
 
-            directories.forEach(function(directory) {
-                var empty = fs.isEmptySync(directory);
-                if (empty === true) {
-                    dependenciesFound = false;
-                }
-            });
-
-            return dependenciesFound;
-        },
-
-        // Travel to project root folder
-        getToRoot: function() {
-            var filesInWorkingDir = fs.readdirSync('./');
-            var atProjectRoot = filesInWorkingDir.indexOf('package.json') !== -1;
-
-            if (atProjectRoot) {
-                return;
-            } else {
-                process.chdir('../');
-                this.getToRoot();
+        directories.forEach(function(directory) {
+            var empty = fs.isEmptySync(directory);
+            if (empty === true) {
+                dependenciesFound = false;
             }
-        }
-    };
+        });
 
-}());
+        return dependenciesFound;
+    },
+
+    // Travel to project root folder
+    getToRoot: function() {
+        var filesInWorkingDir = fs.readdirSync('./');
+        var atProjectRoot = filesInWorkingDir.indexOf('package.json') !== -1;
+
+        if (atProjectRoot) {
+            return;
+        } else {
+            process.chdir('../');
+            this.getToRoot();
+        }
+    }
+};
