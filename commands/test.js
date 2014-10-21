8a1ef230ef1023d5e4cb68ec41621f9ac95326ab
Brittany Storoz <brittanystoroz@gmail.com>
2014-10-21 13:18:18 -0400

    Warn users if bower component test frameworks are missing

--- commands/test.js
+++ commands/test.js
@@ -1,13 +1,27 @@
 'use strict';
 
+var chalk = require('chalk');
+var shell = require('shelljs');
 var spawn = require('child_process').spawn;
+var util = require('../utils');
 
 module.exports = {
     name: 'test',
     description: 'Runs all unit tests in your application',
     run: function() {
-        spawn(this.binaryPath + 'grunt', ['test'], {
-            stdio: 'inherit'
-        });
+        var dependenciesExist = util.checkForDeps([
+            'test/bower_components'
+        ]);
+        if (dependenciesExist === true) {
+            spawn(this.binaryPath + 'grunt', ['test'], {
+                stdio: 'inherit'
+            });
+        } else {
+            console.log(
+                chalk.red('Warning: Test dependencies are missing.') +
+                chalk.yellow('\nTry running `bower install` from the test directory of your application.')
+            );
+            shell.exit(1);
+        }
     }
 };
